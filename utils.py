acfd80f6be8f667162a1e0c9804449e673da77af
linye-boli <linye@changchunboli.com>
2023-11-29 02:17:41 +0000

    modify utils.py add interp1dmat, restrict1dmat

--- utils.py
+++ utils.py
@@ -1,6 +1,48 @@
+import numpy as np
 import torch 
 import torch.nn.functional as F
-from einops import rearrange
+from einops import rearrange, repeat
+
+def interp1d_mat(n, order=2):
+    if order == 2:
+        mat = torch.zeros((2*n+1, n))
+        kernel = torch.tensor([1., 2., 1.])/2
+        klen = 3
+    elif order == 4:
+        mat = torch.zeros((2*n+1+4, n))
+        kernel = torch.tensor([-1., 0, 9, 16, 9, 0, -1.])/16
+        klen = 7
+    elif order == 6:
+        mat = torch.zeros((2*n+1+8, n))
+        kernel = torch.tensor([3., 0, -25, 0, 150, 256, 150, 0, -25, 0, 3])/256
+        klen = 11
+
+    for i in range(n):
+        mat[2*i:2*i+klen, i] = kernel 
+        
+    if order == 4:
+        mat = mat[2:-2]
+    elif order == 6:
+        mat = mat[4:-4]
+
+    return mat
+
+def restrict1d_mat(n, order=2):
+    interpmat = interp1d_mat(n, order)/2
+    return interpmat.T
+
+def interp1d_matmul(vH, interpmat=None, order=2):
+    n = vH.shape[-1]
+    if interpmat is None:
+        interpmat = interp1d_mat(n, order)
+    return torch.einsum('mn, bcn->bcm', interpmat, vH)
+
+def restrict1d_matmul(vh, restrictmat=None, order=2):
+    n = vh.shape[-1]
+    if restrictmat is None:
+        restrictmat = restrict1d_mat((n-1)//2, order)
+    return torch.einsum('mn, bcn->bcm', restrictmat, vh)
+
 
 def interp1d(vH, order=2):
     # vH : (batch, c, H)
@@ -9,8 +51,21 @@ def interp1d(vH, order=2):
         kernel = torch.tensor([[[1., 2., 1.]]]).to(vH)
         w = 1/2 
         s = 2
+        p = 0
+
+    if order == 4:
+        kernel = torch.tensor([[[-1,0,9,16,9,0,-1]]]).to(vH)
+        w = 1/16
+        s = 2
+        p = 2
     
-    vh = w * F.conv_transpose1d(vH, kernel, stride=s)
+    if order == 6:
+        kernel = torch.tensor([[[3, 0, -25, 0, 150, 256, 150, 0, -25, 0, 3]]]).to(vH)
+        w = 1/256
+        s = 2 
+        p = 4
+    
+    vh = w * F.conv_transpose1d(vH, kernel, stride=s, padding=p)
     return  vh 
 
 def restrict1d(vh, order=2):
@@ -19,8 +74,22 @@ def restrict1d(vh, order=2):
         kernel = torch.tensor([[[1., 2., 1.]]]).to(vh)
         w = 1/4
         s = 2
+        p = 0
+    
+    if order == 4:
+        kernel = torch.tensor([[[-1,0,9,16,9,0,-1]]]).to(vh)
+        w = 1/32
+        s = 2
+        p = 2
+    
+    if order == 6:
+        kernel = torch.tensor([[[3, 0, -25, 0, 150, 256, 150, 0, -25, 0, 3]]]).to(vh)
+        w = 1/512
+        s = 2
+        p = 4
+    
 
-    vH = w * F.conv1d(vh, kernel, stride=s)
+    vH = w * F.conv1d(vh, kernel, stride=s, padding=p)
     return vH
 
 def interp1d_cols(KhH, order=2):
@@ -42,38 +111,75 @@ def interp1d_rows(KHh, order=2):
 
     return Khh
 
+def multi_summation(K, u, h):
+    # KHH : (batch, c, m, n)
+    # u : (batch, c, n)
+    # h : float scalar
+    return h * torch.einsum('bcmn, bcn-> bcm', K, u)
 
+def fetch_nbrs(n, m):
+    # n : int, lenth of inputs,
+    # m : int, radius of window
+ 
+    idx_h = torch.arange(1, n+1)
+    idx_nbrs = torch.arange(-m, m+1)
+    idx_j = torch.cartesian_prod(idx_h, idx_nbrs).sum(axis=1).reshape(-1, 2*m+1) # n x 2m+1
+    idx_i = repeat(idx_h, 'i -> i m', m=2*m+1)
+
+    return idx_i, idx_j
+
+def fetch_interpnbrs():
+    pass 
+
+def L1Norm(est, ref):
+    b, c, n = est.shape
+    return (est - ref).abs().sum().item()/(n+1)/b
 
 if __name__ == '__main__':
 
     # test interp1d
-    vH = torch.tensor([[0.1, 0.5], [0.8, -0.2], [-0.4, 0.6]])
-    IHh = torch.tensor([
-        [1., 0., 0.],
-        [2., 0., 0.],
-        [1., 1., 0.],
-        [0., 2., 0.],
-        [0., 1., 1.],
-        [0., 0., 2.],
-        [0., 0., 1.],
-    ])   
+    l = 8
+    n = 2**l - 1
+    lb = 0
+    ub = 2*np.pi
+    xh = torch.linspace(lb, ub, n+2)[1:-1][None][None]
+    xH = xh[:,:,1::2]
+    vh = torch.sin(xh)
+    vH = torch.sin(xH)
 
-    vh_ = 0.5 * IHh @ vH 
-    print('mat result : ')
-    print(vh_)
+    vh_ord2 = interp1d(vH, order=2)
+    vh_ord4 = interp1d(vH, order=4)
+    vh_ord6 = interp1d(vH, order=6)
 
-    vH = rearrange(vH, 'n b -> b 1 n')
-    vh_ = interp1d(vH, order=2)
-    print('deconv result : ')
-    print(vh_[:,0].T)
+    vh_ord2_mat = interp1d_matmul(vH, order=2)
+    vh_ord4_mat = interp1d_matmul(vH, order=4)
+    vh_ord6_mat = interp1d_matmul(vH, order=6)
 
-    vh = torch.tensor([
-        [0.1, 0.5], [0.8, -0.2], [-0.4, 0.6], [0.1, 0.5], [0.8, -0.2],[-0.4, 0.6], [0.1, 0.5]])
-    vH_ = 0.25 * IHh.T @ vh
-    print('mat result : ')
-    print(vH_)
+    vH_ord2 = restrict1d(vh, order=2)
+    vH_ord4 = restrict1d(vh, order=4)
+    vH_ord6 = restrict1d(vh, order=6)
 
-    vh = rearrange(vh, 'n b -> b 1 n')
-    vH_ = restrict1d(vh, order=2)
-    print('conv result : ')
-    print(vH_[:,0].T)
+    vH_ord2_mat = restrict1d_matmul(vh, order=2)
+    vH_ord4_mat = restrict1d_matmul(vh, order=4)
+    vH_ord6_mat = restrict1d_matmul(vh, order=6)
+
+    print('deconv interp error(L1Norm) : ')
+    print('ord2 : ', L1Norm(vh_ord2,vh))
+    print('ord4 : ', L1Norm(vh_ord4,vh))
+    print('ord6 : ', L1Norm(vh_ord6,vh))
+    
+    print('matmul interp error : ')
+    print('ord2 : ', L1Norm(vh_ord2_mat,vh))
+    print('ord4 : ', L1Norm(vh_ord4_mat,vh))
+    print('ord6 : ', L1Norm(vh_ord6_mat,vh))
+    
+    print('conv restrict error : ')
+    print('ord2 : ', L1Norm(vH_ord2,vH))
+    print('ord4 : ', L1Norm(vH_ord4,vH))
+    print('ord6 : ', L1Norm(vH_ord6,vH))
+    
+    print('matmul restrict error : ')
+    print('ord2 : ', L1Norm(vH_ord2_mat,vH))
+    print('ord4 : ', L1Norm(vH_ord4_mat,vH))
+    print('ord6 : ', L1Norm(vH_ord6_mat,vH))
+    
