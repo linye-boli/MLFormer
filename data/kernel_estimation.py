faeba38ee0826fa1fb9948d82b99ae0f2aaa28fd
linye-boli <linye@changchunboli.com>
2023-12-04 08:53:40 +0000

    fix injection, interpolation, and boundary problem

--- data/kernel_estimation.py
+++ data/kernel_estimation.py
@@ -8,10 +8,11 @@ from dataset import numerical_kernel
 
 l = 13
 us = scipy.io.loadmat(
-    '/workdir/MLFormer/data/kernel_estimation/grf1d_N2000_s8193_m0_gamma1_tau1_sigma1_dirichlet.mat')['f'][:,1:-1]
+    '/workdir/MLFormer/data/kernel_estimation/grf1d_N2000_s8193_m0_gamma1_tau1_sigma1_dirichlet.mat')['f']
 kernel_types = ['cosine', 'lnabs', 'laplace', 'advection_diffusion', 'helmholtz', 'negative_helmholtz']
 
 for kernel in tqdm(kernel_types, total=len(kernel_types)):
     Khh, xh, h = numerical_kernel(l, kernel_type=kernel)
     ws = np.einsum('mn,bn->bm', Khh[0,0].numpy(), us) * h
-    np.save('/workdir/MLFormer/data/kernel_estimation/{:}_8191.npy'.format(kernel), ws)
+    np.save('/workdir/MLFormer/data/kernel_estimation/{:}_8193.npy'.format(kernel), ws)
+
